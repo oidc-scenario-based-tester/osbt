2c729210ef62108e46e24ee5d267fa4b25a8f9a1
melonattacker <junkitombob@gmail.com>
2023-08-04 15:05:00 +0900

    UPDATE: cli

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"time"
 
 	"github.com/oidc-scenario-based-tester/osbt/server"
 	"github.com/spf13/cobra"
 )
-
-const resultsFile = "osbt.log"
 
 func main() {
 	var rootCmd = &cobra.Command{Use: "osbt"}
@@ -31,7 +31,7 @@ func main() {
 	runCmd.Flags().StringP("file", "f", "", "Specify the test file to run")
 	runCmd.Flags().StringP("dir", "d", "", "Specify the test directory to run all tests")
 	runCmd.Flags().BoolP("recursive", "r", false, "Search directories recursively")
-	runCmd.Flags().StringP("output", "o", "", "Specify the output file to store the test results")
+	runCmd.Flags().StringP("timeout", "t", "30s", "Specify the timeout for running tests")
 
 	rootCmd.AddCommand(runCmd, serverCmd)
 	rootCmd.Execute()
@@ -41,54 +41,53 @@ func runTests(cmd *cobra.Command, args []string) {
 	file, _ := cmd.Flags().GetString("file")
 	dir, _ := cmd.Flags().GetString("dir")
 	recursive, _ := cmd.Flags().GetBool("recursive")
-	output, _ := cmd.Flags().GetString("output")
+	timeout, _ := cmd.Flags().GetString("timeout")
+
+	timeoutDuration, err := time.ParseDuration(timeout)
+	if err != nil {
+		fmt.Printf("Invalid timeout value: %v", err)
+		os.Exit(1)
+	}
 
 	if file != "" {
-		result, _ := runTest(file)
-		saveResults(resultsFile, result)
-		if output != "" {
-			saveResults(output, result)
-		}
+		runTest(file, timeoutDuration)
 	}
 
 	if dir != "" {
-		runTestsInDir(dir, recursive, output)
+		runTestsInDir(dir, recursive, timeoutDuration)
 	}
 }
 
-func runTest(file string) (string, error) {
-	cmd := exec.Command("python", file)
+func runTest(file string, timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "python", file)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		return "", err
+		if ctx.Err() == context.DeadlineExceeded {
+			fmt.Printf("Timeout running test: %s\n", file)
+		} else {
+			fmt.Printf("Error running test: %v\n", err)
+		}
+	} else {
+		fmt.Printf("Test result: %s\n", out.String())
 	}
-	return out.String(), nil
 }
 
-func runTestsInDir(dir string, recursive bool, output string) {
+func runTestsInDir(dir string, recursive bool, timeout time.Duration) {
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() && filepath.Ext(path) == ".py" {
-			result, _ := runTest(path)
-			saveResults(resultsFile, result)
-			if output != "" {
-				saveResults(output, result)
-			}
+			runTest(path, timeout)
 		}
 		return nil
 	})
 	if err != nil {
-		fmt.Println("Error running tests:", err)
+		fmt.Printf("Error running tests in directory: %v\n", err)
 	}
-}
-
-func saveResults(file string, result string) {
-	f, _ := os.OpenFile(file, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	defer f.Close()
-	f.WriteString(result)
-	f.WriteString("\n")
 }
